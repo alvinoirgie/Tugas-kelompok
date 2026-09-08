72ffe7ed1bc9774307fae9355ac3d41e68aa155e
IRGIEEZZ <alvinoirgie@gmail.com>
2026-09-08 17:07:54 +0700

    TAMBAHAN WELCOME

--- app/src/main/java/com/example/tugaskelompok/MainActivity.java
+++ app/src/main/java/com/example/tugaskelompok/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.tugaskelompok;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnBurger, btnAyam, btnMie, btnPizza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnBurger = findViewById(R.id.btnBurger);
+        btnAyam = findViewById(R.id.btnAyam);
+        btnMie = findViewById(R.id.btnMie);
+        btnPizza = findViewById(R.id.btnPizza);
+
+        btnBurger.setOnClickListener(v -> {
+            Toast.makeText(this,
+                    "Burger Spesial dipilih!",
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        btnAyam.setOnClickListener(v -> {
+            Toast.makeText(this,
+                    "Ayam Geprek dipilih!",
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        btnMie.setOnClickListener(v -> {
+            Toast.makeText(this,
+                    "Mie Goreng dipilih!",
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        btnPizza.setOnClickListener(v -> {
+            Toast.makeText(this,
+                    "Pizza dipilih!",
+                    Toast.LENGTH_SHORT).show();
         });
     }
 }
